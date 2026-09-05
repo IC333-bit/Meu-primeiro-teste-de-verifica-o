865196c2a4f43a637350f42304b70d00c5218c8f
IC333-bit <henriquefernandes939@gmail.com>
2026-09-05 19:06:21 -0300

    segue o projeto que eu fiz
    Projeto simples para testar a conexão entre frontend e backend. Estou começando na jornada full stack — já tenho experiência com Python e Java, mas aqui usei Python (Flask) no backend e HTML na interface, ainda em aprendizado. Objetivo: entender como um formulário envia dados a um servidor Python. Sugestões são bem-vindas!

---
+++ Projeto/backend/backend.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def pagina_login():
+    return render_template('index.html')
+
+@app.route('/enviar', methods=['POST'])
+def enviar():
+    texto = request.form['nome']
+    print("Recebi do front end:", texto)
+    if texto.lower() =='henrique' or texto.lower() == 'henrique':
+        return f'Ola admin'
+    else:
+        return f"Voce é gay"
+
+if __name__ == '__main__':
+    app.run(debug=True)
